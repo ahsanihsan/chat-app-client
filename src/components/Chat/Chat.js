d146a59b55ed991eab339230270aa8c1fc42a741
Ahsan Ihsan <ahsan.ihsan@outlook.com>
2021-08-09 19:33:37 +0500

    Added chat component with socket.io-client to connect/disconnect with the connection

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+  const [room, setRoom] = useState("");
+  const [name, setName] = useState("");
+  const ENDPOINT = "localhost:8080";
+
+  useEffect(() => {
+    const { room, name } = queryString.parse(location.search);
+
+    setRoom(room);
+    setName(name);
+
+    socket = io(ENDPOINT);
+    socket.emit("join", { name, room }, (error) => {
+      alert(error.error);
+    });
+
+    return () => {
+      socket.disconnect({ name, room });
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
+
   return <div>Chat</div>;
 };
 
